d1f1272c5ccbdf37239cfacc36c042cd37f9e6d5
Tessa van de Langenberg <bliksem@wmmt.nl>
2024-12-14 19:07:53 +0100

    praktische informatie pagina toevoegen

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function showNav(){
     var hidden = document.getElementById("other_pages");
     if (hidden.style.display === "block"){
@@ -6,4 +5,20 @@ function showNav(){
     } else {
         hidden.style.display = "block";
     }
+}
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
 }
