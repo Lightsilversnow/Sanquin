344e5d8a5697283fa7dc41d2e8860a92717e21b4
Tessa van de Langenberg <bliksem@wmmt.nl>
2024-12-14 16:47:56 +0100

    navbar praktische informatie pagina

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function showNav(){
+    var hidden = document.getElementById("other_pages");
+    if (hidden.style.display === "block"){
+        hidden.style.display = "none";
+    } else {
+        hidden.style.display = "block";
+    }
+}
